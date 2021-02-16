22e0ec833a14c747152793b7f3b1957462538302
Lluís Suau <78743468+LluisPecos@users.noreply.github.com>
2021-02-16 20:31:10 +0100

    Apartado 11
    Cambiar imagen home en nueva rama

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 
 <div id="inicio" class="row">
     <h1 class="text-center w-100 mb-4">¡Bienvenido a mi página web!</h1>
-    <img style="width: 100%;" src="https://images.pexels.com/photos/6652651/pexels-photo-6652651.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+    <img style="width: 100%;" src="https://images.pexels.com/photos/258112/pexels-photo-258112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
     <div class="col-12">
         
     </div>
