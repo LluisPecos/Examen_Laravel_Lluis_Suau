9ea83bdda7f75d9388ea3216e0b791eb833ad8a6
Lluís Suau <78743468+LluisPecos@users.noreply.github.com>
2021-02-16 19:44:51 +0100

    Apartado 6
    Imágen home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,8 @@
 @section('content')
 
 <div id="inicio" class="row">
-    <h1 class="text-center">¡Bienvenido a mi página web!</h1>
+    <h1 class="text-center w-100 mb-4">¡Bienvenido a mi página web!</h1>
+    <img style="width: 100%;" src="https://images.pexels.com/photos/6652651/pexels-photo-6652651.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
     <div class="col-12">
         
     </div>
